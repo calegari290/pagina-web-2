34cdecefc01e8e188c87a54ca55e7577a39238c6
calegari290 <joao.galindo.calegari@escola.pr.gov.br>
2025-06-12 07:45:46 -0300

    76

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,7 @@ criaCartao(
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Geografia',
+    'Qual a capital do EUA?',
+    '(Washington,)'
 )
